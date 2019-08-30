4b90189e0df0e7461b0ac374abe88ff052812230
swb <13019016429@163.com>
2019-08-31 00:32:17 +0800

    dat2last

--- Desktop/project/views/public/js/index.js
+++ Desktop/project/views/public/js/index.js
@@ -13,12 +13,13 @@ setInterval(
         $('.stepRight img').eq(correct).addClass('active')
             // console.log($('.stepRight img'))
     }, 4000)
-var current = 0
+
 var currents = 0
-var arr = ['https://static.jx3.xoyo.com/gfsj/20181201/2plxj.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/3light.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/4beach.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/5whhh.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/6jzz.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/1night.mp4']
+var arr = ['https://static.jx3.xoyo.com/gfsj/20181201/1night.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/2plxj.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/3light.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/4beach.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/5whhh.mp4', 'https://static.jx3.xoyo.com/gfsj/20181201/6jzz.mp4']
 
 
 
+var index1;
 // clearInterval(timer)
 // var index = $(".vid_two_tits div").index($(this));
 // $('.vid_two_tits div').removeClass('act')
@@ -27,13 +28,12 @@ var arr = ['https://static.jx3.xoyo.com/gfsj/20181201/2plxj.mp4', 'https://stati
 var timer = setInterval(function() {
     $(".video-box").fadeOut('fast') //第一次视频结束进入定时器，给包裹着video的父盒子淡出效果。
     $(".video-box video").empty('source') //清除video下的source标签
-    current %= 6 //保证current的值为0-5;
 
-    var src = arr[current]
-
-    current++
     currents++
     currents %= 6
+    var src = arr[currents]
+
+
     sourceDom = $("<source src=\"" + src + "\">"); //获取动态src添加到 source标签内
     $(".video-box video").append(sourceDom) //将sourceDom插入到video
     $(".video-box video")[0].load(); //让video重新加载
@@ -45,19 +45,78 @@ var timer = setInterval(function() {
     $('.vid_two_tits div').eq(currents).addClass('act')
 
 }, 5300);
-// =====================14大门派=========================
-var curren = 0
-var timer = setInterval(function() {
 
+$('.vid_two_tits div').click(function(e) {
+        // clearInterval(timer1)
+
+        index1 = $(".vid_two_tits div").index($(this));
+
+        // $('.menpai_pro').eq(index).fadeOut('slow')
+        $(".video-box video").empty('source')
+        var src = arr[index1]
+        sourceDom = $("<source src=\"" + src + "\">"); //获取动态src添加到 source标签内
+        $(".video-box video").append(sourceDom) //将sourceDom插入到video
+        $(".video-box video")[0].load(); //让video重新加载
+        // $(".video-box").show();
+        // 自动播放
+        $(".video-box video")[0].play()
+
+        $('.vid_two_tits div').removeClass('act')
+        $(this).addClass('act')
+        $('.menpai_pro').removeClass('act_menpai')
+        $('.menpai_pro').eq(index).addClass('act_menpai')
+
+        currents = index1;
+    })
+    // =====================14大门派=========================
+var curren = 0
+
+
+
+var index;
+
+
+var timer1 = setInterval(function() {
     curren++
     curren %= 14
     $('.menpai_pro').removeClass('act_menpai')
     $('.menpai_pro').eq(curren).addClass('act_menpai')
     $('.menpai_con .tit div').removeClass()
     $('.menpai_con .tit div').eq(curren).addClass('act')
+    $('.menpai_pro').eq(index).fadeOut('fast')
     $('.menpai_pro').eq(curren).fadeOut('fast')
+
     $('.menpai_pro').eq(curren).fadeIn('slow')
-
-
-
 }, 5500);
+
+
+
+$('.menpai_con .tit div').click(function(e) {
+        // clearInterval(timer1)
+
+        index = $(".menpai_con .tit div").index($(this));
+
+        // $('.menpai_pro').eq(index).fadeOut('slow')
+
+
+        $('.menpai_con .tit div').removeClass('act')
+        $(this).addClass('act')
+        $('.menpai_pro').removeClass('act_menpai')
+        $('.menpai_pro').eq(index).addClass('act_menpai')
+
+        curren = index;
+    })
+    // ==============旋转===============
+
+
+var rotation2 = function() {
+    $('#menpai2 .rot').rotate({
+        angle: 0,
+        animateTo: 360,
+        callback: rotation2,
+        easing: function(x, t, b, c, d) {
+            return c * (t / d) + b;
+        }
+    });
+}
+rotation2();
