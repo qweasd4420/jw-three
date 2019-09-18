c718441926fdd7ed4ba721fb68c4e9b511ebed4d
杨青 <yangqing@163.com>
2019-09-18 17:56:02 +0800

    初始化px-common

--- src/utils/UserMockApi.js
+++ src/utils/UserMockApi.js
@@ -1,5 +1,5 @@
-
-const UserBaseApi = 'http://localhost:9529/mock'
+// const UserBaseApi = 'http://localhost:9527/mock'
+// const UserBaseApi = 'http://localhost:9527/mock'
 /**
  * 课程Api
  * @author 杨文培
@@ -9,6 +9,18 @@ export default class UserMockApi {
    * mock地址
    */
   static getUserMockApi() {
-    return UserBaseApi
+    const ip_addr = document.location.hostname
+    const port = document.location.port
+    if (process.env.NODE_ENV === 'development') {
+      console.log('development')
+      return 'http://' + ip_addr + ':' + port + '/mock'
+    } else {
+      console.log('production')
+      return '/px-common-admin'
+    }
   }
+  // static getUserMockApi() {
+  //   const url = 'http://192.100.2.49:10002/px-common-admin'
+  //   return url
+  // }
 }
